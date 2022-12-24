f7acb126f4920ed6f902b09ca7a48edff02e9b53
Anh Ky Nguyen <anh163@gmail.com>
2022-12-24 15:59:25 +0700

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from "./App"
-import { createRoot } from 'react-dom/client';
-
+import React from "react";
+import App from "./App";
+import { createRoot } from "react-dom/client";
 
 const container = document.getElementById("root");
-const root = createRoot(container)
-root.render(<App/>);
+const root = createRoot(container);
+root.render(<App />);
